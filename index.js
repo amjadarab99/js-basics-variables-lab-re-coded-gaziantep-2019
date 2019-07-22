66a7bd2a53349e244b687d59f772dc4bc8b4a183
Amjad Arab <amjad.arab99@gmail.com>
2019-07-22 09:59:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const companyName = "Scuber";
 
+<<<<<<< HEAD
 let mostProfitableNeighborhood = "Chelsea";
 
 let companyCeo = "Susan Smith";
+=======
+let 
+>>>>>>> 74bc6ff989998aff6cb37003da5c587d6403519b
